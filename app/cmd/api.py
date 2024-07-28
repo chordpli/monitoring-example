bceea5aaafa1e0cebd9df893ae1fb5b14663cc26
pli <chordplaylist@gmail.com>
2024-07-28 19:30:58 +0900

    feat: add opentelemetry

--- app/cmd/api.py
+++ app/cmd/api.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExporter
+
 from app.api.test.routers.test import router as test_router
 
 app = FastAPI()
+
+tracer = TracerProvider()
+tracer.add_span_processor(
+    SimpleSpanProcessor(ConsoleSpanExporter())
+)
+FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
+
 
 app.include_router(test_router)
